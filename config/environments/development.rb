92c414aa14f1cb54ce2573851b1860b1019b5ce8
bodrovis <bodrovis@protonmail.com>
2017-07-11 17:09:40 +0300

    config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  config.action_cable.url = 'ws://localhost:3000/cable'
+  config.action_cable.allowed_request_origins = [ 'http://localhost:3000', 'http://127.0.0.1:3000' ]
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  mount ActionCable.server => '/cable'
   root 'chats#index'
 end
